04e10d4d78fd6425a281847b5557cdd5027f7b64
Mikhail <evteev.mikhail.a@gmail.com>
2020-06-30 14:15:43 +0300

    Перезаливка проекта

---
+++ app/src/main/java/com/refactoring/noteorganizerproject/notes/Note.java
@@ -0,0 +1,10 @@
+package com.refactoring.noteorganizerproject.notes;
+
+public class Note {
+    public long id;
+    public String title;
+    public String body;
+    public int cardImageUri;
+    public long datatime;
+    public int noteColor = 0;
+}
