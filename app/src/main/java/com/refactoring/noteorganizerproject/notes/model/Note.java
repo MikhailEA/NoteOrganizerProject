5eb14d919db9e13471afa9dc70fed677e8d34b51
Mikhail <evteev.mikhail.a@gmail.com>
2020-07-08 20:44:05 +0300

    Структура блокнота

---
+++ app/src/main/java/com/refactoring/noteorganizerproject/notes/model/Note.java
@@ -0,0 +1,40 @@
+package com.refactoring.noteorganizerproject.notes;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Note {
+    public long id;
+    public String title;
+    public String body;
+    public int cardImageUri;
+    public long dataTime;
+    public int noteColor = 0;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", cardImageUri=" + cardImageUri +
+                ", dataTime=" + dataTime +
+                ", noteColor=" + noteColor +
+                '}';
+    }
+
+    public boolean isEmpty() {
+        return (this.body == null || this.body.length() == 0)
+                && (this.title == null || this.title.length() == 0);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        Note other = (Note) obj;
+        if (obj == null) return false;
+        return this.title.equals(other.title) && this.body.equals(other.body) && dataTime == other.dataTime;
+    }
+
+
+}
