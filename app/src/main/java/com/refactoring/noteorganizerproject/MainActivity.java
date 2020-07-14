06a428bd70f7f651e740cc6b5ed69248328c341c
Mikhail <evteev.mikhail.a@gmail.com>
2020-07-14 15:50:22 +0300

    Добавление цветов

--- app/src/main/java/com/refactoring/noteorganizerproject/MainActivity.java
+++ app/src/main/java/com/refactoring/noteorganizerproject/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        //setAppTheme();
+        setAppNavigation();
+        //hideKeyBoard();
+    }
+
+    private void setAppNavigation() {
+        BottomNavigationView navigationView = findViewById(R.id.navigation_view);
     }
 }
