4cddb822e3dc8e007f521860e7f5be1d1c95c5b6
Mikhail <evteev.mikhail.a@gmail.com>
2020-07-16 20:41:38 +0300

    Работа с классами листов фрагмента

--- app/src/main/java/com/refactoring/noteorganizerproject/notes/notes_list_fragment/presenter/INotesPresenter.java
+++ app/src/main/java/com/refactoring/noteorganizerproject/notes/notes_list_fragment/presenter/INotesPresenter.java
@@ -1,4 +1,4 @@
 package com.refactoring.noteorganizerproject.notes.notes_list_fragment.view.recycler;
 
-public class NotesFragment {
+public interface INotesPresenter {
 }
--- app/src/main/java/com/refactoring/noteorganizerproject/notes/notes_list_fragment/view/NotesFragmentInitialize.java
+++
@@ -1,6 +0,0 @@
-package com.refactoring.noteorganizerproject.notes.notes_list_fragment.view;
-
-import androidx.fragment.app.Fragment;
-
-public class NotesFragmentInitialize extends Fragment {
-}
