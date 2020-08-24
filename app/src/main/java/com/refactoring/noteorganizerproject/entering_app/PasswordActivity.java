0a298fdb2945212913b9930a81f3371a3a00e1f5
Fadseres <evteev.mikhail.a@gmail.com>
2020-08-24 18:13:37 +0300

    Рефакторинг

--- app/src/main/java/com/refactoring/noteorganizerproject/entering_app/PasswordActivity.java
+++ app/src/main/java/com/refactoring/noteorganizerproject/entering_app/PasswordActivity.java
@@ -1,8 +1,11 @@
 package com.refactoring.noteorganizerproject.entering_app;
 
+import android.os.Bundle;
+
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.refactoring.noteorganizerproject.BaseActivity;
+import com.refactoring.noteorganizerproject.R;
 import com.refactoring.noteorganizerproject.entering_app.presenter.PasswordPresenter;
 
 public class PasswordActivity extends BaseActivity {
@@ -11,6 +14,13 @@ public class PasswordActivity extends BaseActivity {
     MaterialButton confirm;
 
     PasswordPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setAppTheme();
+        setContentView(R.layout.activity_password);
+    }
 
 
 }
--- app/src/main/java/com/refactoring/noteorganizerproject/entity/shared_prefs/SharedPreferencesManager.java
+++ app/src/main/java/com/refactoring/noteorganizerproject/entity/shared_prefs/SharedPreferencesManager.java
@@ -13,60 +13,97 @@ public class SharedPreferencesManager {
         sp = context.getSharedPreferences("app_settings", Context.MODE_PRIVATE);
     }
 
-    public long getClickedNoteId() { return getLong("clickedNoteId", -1L); }
-    public void setClickedNoteId(long id) { putLong("clickedNoteId", id); }
-    public long getClickedTodoId() {return getLong("clickedTodoId", -1L); }
-    public void setClickedTodoId(long id) {putLong("clickedTodoId", id); }
+    public long getClickedNoteId() {
+        return getLong("clickedNoteId", -1L);
+    }
+    public void setClickedNoteId(long id) {
+        putLong("clickedNoteId", id);
+    }
+    public long getClickedTodoId() {
+        return getLong("clickedTodoId", -1L);
+    }
+    public void setClickedTodoId(long id) {
+        putLong("clickedTodoId", id);
+    }
 
     // theme
+    public int getAppTheme() {
+        return getInt("appTheme", R.style.AppTheme);
+    }
+    public void setAppTheme(int appThemeId) {
+        putInt("appTheme", appThemeId);
+    }
 
-    public int getAppTheme() { return getInt("appTheme", R.style.AppTheme); }
-    public void setAppTheme(int appThemeId) {putInt("appTheme", appThemeId);}
-
-
+    // sync with storage
+    public void setLastSyncDate(Long timeInMills) {
+        putLong("lastSyncWithStorage", timeInMills);
+    }
+    public Long getLastSyncDate() {
+        return getLong("lastSyncWithStorage", 0L);
+    }
+    // data directory
+    public String getAppDataDirectory() {
+        return getString("appDataDirectory", "");
+    }
     public void setAppDataDirectory(String path) {
         putString("appDataDirectory", path);
     }
 
-    // BASIC FOR SP
-
-    private Long getString(String key, Long defaultValue) {
-        return sp.getLong(key, defaultValue);
+    public boolean isAddingNoteForFirsTime() {
+        return getBoolean("isAddingNoteForFirsTime", true);
+    }
+    public void setAddingNoteForFirsTime(boolean value) {
+        putBoolean("isAddingNoteForFirsTime", value);
     }
 
+    // local password
+    public boolean enterOnPassword() {
+        return getBoolean("enterOnPassword", false);
+    }
+    public void setEnterOnPassword(boolean toEnterOnPassword) {
+        putBoolean("enterOnPassword", toEnterOnPassword);
+    }
+    public String getLocalPassword() {
+        return getString("password", "");
+    }
+    public void setLocalPassword(String password) {
+        putString("password", password);
+    }
+
+
+
+    // BASIC FOR SP
+    private Boolean getBoolean(String key, boolean defaultValue) {
+        return sp.getBoolean(key, defaultValue);
+    }
+    private String getString(String key, String defaultValue) {
+        return sp.getString(key, defaultValue);
+    }
     private Long getLong(String key, Long defaultValue) {
         return sp.getLong(key, defaultValue);
     }
-
-    private Long getInt(String key, Long defaultValue) {
-        return sp.getLong(key, defaultValue);
-    }
-
-    private Long getBoolean(String key, Long defaultValue) {
-        return sp.getLong(key, defaultValue);
-    }
-
-    private void putString(String key, String value) {
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString(key, value);
-        editor.apply();
-    }
-
-    private void putLong(String key, Long value) {
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putLong(key, value);
-        editor.apply();
-    }
-
-    private void putInt(String key, Integer value) {
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt(key, value);
-        editor.apply();
+    private Integer getInt(String key, Integer defaultValue) {
+        return sp.getInt(key, defaultValue);
     }
 
     private void putBoolean(String key, boolean value) {
         SharedPreferences.Editor editor = sp.edit();
         editor.putBoolean(key, value);
+        editor.apply();
+    }
+    private void putString(String key, String value) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+    private void putLong(String key, Long value) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putLong(key, value);
+        editor.apply();
+    }
+    private void putInt(String key, Integer value) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(key, value);
         editor.apply();
     }
 }
