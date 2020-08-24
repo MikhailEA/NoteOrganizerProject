64df99d7a694c130e35db064363ea3425c369050
Fadseres <evteev.mikhail.a@gmail.com>
2020-08-24 18:49:05 +0300

    Продолжение..

--- app/src/main/java/com/refactoring/noteorganizerproject/entering_app/EntranceActivity.java
+++ app/src/main/java/com/refactoring/noteorganizerproject/entering_app/EntranceActivity.java
@@ -1,10 +1,15 @@
 package com.refactoring.noteorganizerproject.entering_app;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.refactoring.noteorganizerproject.BaseActivity;
+import com.refactoring.noteorganizerproject.MainActivity;
 import com.refactoring.noteorganizerproject.R;
 import com.refactoring.noteorganizerproject.entity.AppConfig;
 import com.refactoring.noteorganizerproject.entity.shared_prefs.SharedPreferencesManager;
@@ -25,5 +30,40 @@ public class EntranceActivity extends BaseActivity {
 
     private void initUI() {
         ImageView launcherIcon = findViewById(R.id.launcher_icon);
+        TextView appNameTv = findViewById(R.id.app_name);
+        TextView teamCopyright = findViewById(R.id.team_copyright);
+        resetAlpha(launcherIcon);
+        resetAlpha(appNameTv);
+        resetAlpha(launcherIcon);
+        setAnimation(launcherIcon);
+        setAnimation(appNameTv);
+        setAnimation(teamCopyright);
+    }
+
+    private void enterApp() {
+        boolean passwordEnable = appSettings.enterOnPassword();
+        startActivity(new Intent(this,
+                passwordEnable ? PasswordActivity.class : MainActivity.class));
+        this.finish();
+    }
+
+    private void resetAlpha(View v) {
+        v.setAlpha(0f);
+    }
+
+    private void setAnimation(View v) {
+        float alphaTo = 1f;
+        long duration = 1000;
+
+        v.animate().alpha(alphaTo)
+                .setDuration(duration)
+                .setListener(null);
+    }
+
+    private void initEnteringAppTimer() {
+        Handler delayedOpen = new Handler();
+        delayedOpen.postDelayed(
+                this::enterApp, 1000
+        );
     }
 }
