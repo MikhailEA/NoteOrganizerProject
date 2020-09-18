2c8b6755f72cafc889edfdd2b8cb89e9ca013e6f
Fadseres <evteev.mikhail.a@gmail.com>
2020-09-18 18:19:31 +0300

    Дополнение

--- app/src/main/java/com/refactoring/noteorganizerproject/todos/todos_faragment/TodoRequestCodes.java
+++ app/src/main/java/com/refactoring/noteorganizerproject/todos/todos_faragment/TodoRequestCodes.java
@@ -1,4 +1,9 @@
 package com.refactoring.noteorganizerproject.todos.todos_faragment;
 
 public interface TodoRequestCodes {
+    int REQUEST_NEW_TODO = 1;
+    int REQUEST_EDIT_TODO = 0;
+
+    int ACTION_UPDATE = 1;
+    int ACTION_DELETE = -1;
 }
